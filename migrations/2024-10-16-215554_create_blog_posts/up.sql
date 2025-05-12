833f00a9e9f767b0a1c16a4fc0457c6e87f816a2
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-12 10:30:32 +0300

    Updated migration and the toml file for the migration

--- migrations/2024-10-16-215554_create_blog_posts/up.sql
+++ migrations/2024-10-16-215554_create_blog_posts/up.sql
@@ -4,8 +4,8 @@ CREATE TABLE posts
 (
     id       INTEGER NOT NULL PRIMARY KEY,
     body     TEXT    NOT NULL,
-    date     TEXT    NOT NULL,
+    date     DATE    NOT NULL,
     image    BLOB,
-    username VARCHAR NOT NULL,
+    username VARCHAR(200) NOT NULL,
     avatar   BLOB
 );
