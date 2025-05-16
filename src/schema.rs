74fb8030758b7c1dff7470860717b5641eaed468
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-16 11:00:40 +0300

    Automatic schema.rs update

--- src/schema.rs
+++ src/schema.rs
@@ -7,6 +7,20 @@ diesel::table! {
         date -> Date,
         image -> Nullable<Binary>,
         username -> Text,
+    }
+}
+
+diesel::table! {
+    users (username) {
+        username -> Text,
+        password -> Text,
         avatar -> Nullable<Binary>,
     }
 }
+
+diesel::joinable!(posts -> users (username));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
