203724178262c7437328e56e17f60302477598c8
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-22 14:20:48 +0300

    Add fetch and create function in user repository

--- src/model/user.rs
+++ src/model/user.rs
@@ -1,12 +1,13 @@
-use diesel::{Queryable, Selectable};
+use diesel::{Queryable, Selectable, Insertable};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Queryable, Selectable)]
+#[derive(Serialize, Deserialize, Queryable, Selectable, Insertable)]
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 #[diesel(primary_key(username))]
 pub struct User {
     pub username: String,
+    #[serde(skip_serializing)]
     pub password: String,
     pub avatar: Option<Vec<u8>>,
 }
--- src/repository/user.rs
+++ src/repository/user.rs
@@ -1,29 +1,55 @@
+use crate::error::AppResult;
+use crate::model::user::User;
 use deadpool_diesel::sqlite::{Manager, Object};
 use deadpool_diesel::Pool;
+use diesel::QueryDsl;
+use diesel::{ExpressionMethods, OptionalExtension};
 use diesel::{RunQueryDsl, SelectableHelper};
-use diesel::query_dsl::select_dsl::SelectDsl;
-use crate::error::AppError;
-use crate::model::user::User;
 
 pub struct UserRepository {
-    connection_error: Pool<Manager, Object>,
+    connection_pool: Pool<Manager, Object>,
 }
 
 impl UserRepository {
-    pub fn new(connection_error: Pool<Manager, Object>) -> Self {
-        Self { connection_error }
+    pub fn new(connection_pool: Pool<Manager, Object>) -> Self {
+        Self { connection_pool }
     }
 
-    pub async fn fetch_all_users(&self) -> Result<Vec<User>, AppError> {
+    pub async fn fetch_all_users(&self) -> AppResult<Vec<User>> {
         use crate::schema::users::dsl::*;
 
-        let conn = self.connection_error.get().await?;
+        let conn = self.connection_pool.get().await?;
 
-        let results = conn.interact(|conn| {
-            users.select(User::as_select())
-            .load::<User>(conn)
-        }).await??;
+        let results = conn
+            .interact(|conn| users.select(User::as_select()).load::<User>(conn))
+            .await??;
 
         Ok(results)
+    }
+
+    pub async fn create_new_user(&self, user: User) -> AppResult<()> {
+        use crate::schema::users::dsl::*;
+        let conn = self.connection_pool.get().await?;
+
+        conn.interact(|conn| diesel::insert_into(users).values(user).execute(conn))
+            .await??;
+        Ok(())
+    }
+
+    pub async fn get_user_by_username(&self, username: String) -> AppResult<Option<User>> {
+        use crate::schema::users::dsl::users;
+        let conn = self.connection_pool.get().await?;
+
+        let result = conn
+            .interact(|conn| {
+                users
+                    .find(username)
+                    .select(User::as_select())
+                    .first(conn)
+                    .optional()
+            })
+            .await??;
+
+        Ok(result)
     }
 }
