fa386f1bcdc5c6360db8c87585db4007a179813f
AleXutzZu <davidalexrobertyt@gmail.com>
2025-06-05 15:09:48 +0300

    Add formatting to the displayed date

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {Link} from "react-router";
+import {format} from "date-fns";
 
 export interface IPost {
     id: number,
@@ -17,7 +18,7 @@ export default function Post(props: IPost) {
         <div className="relative max-w-md lg:max-w-2xl mx-auto my-6 p-4 rounded-2xl shadow-lg bg-white min-w-xs">
             <h2 className="text-2xl font-semibold">{props.title}</h2>
             <p className="text-sm mb-2">by <Link to={`/user/${props.username}`}
-                                                 className="italic text-sm">{props.username}</Link> - {props.date}</p>
+                                                 className="italic text-sm">{props.username}</Link> - {format(props.date, "MMM do yyyy")}</p>
             <div
                 className={`transition-all duration-500 ${
                     expanded ? "blur-0 h-auto" : "blur-[2px] max-h-60 overflow-hidden"
