4135850df8718fdff5b750ddc9c2e40e12d79fa3
AleXutzZu <davidalexrobertyt@gmail.com>
2025-06-05 14:38:40 +0300

    Add component to render a post

---
+++ frontend/src/components/Post.tsx
@@ -0,0 +1,49 @@
+import {useState} from "react";
+import {Link} from "react-router";
+
+export interface IPost {
+    id: number,
+    title: string,
+    body: string,
+    date: Date,
+    username: string,
+}
+
+export default function Post(props: IPost) {
+    const [expanded, setExpanded] = useState(false);
+    const [imageError, setImageError] = useState(false);
+
+    return (
+        <div className="relative max-w-md lg:max-w-2xl mx-auto my-6 p-4 rounded-2xl shadow-lg bg-white min-w-xs">
+            <h2 className="text-2xl font-semibold">{props.title}</h2>
+            <p className="text-sm mb-2">by <Link to={`/user/${props.username}`}
+                                                 className="italic text-sm">{props.username}</Link> - {props.date}</p>
+            <div
+                className={`transition-all duration-500 ${
+                    expanded ? "blur-0 h-auto" : "blur-[2px] max-h-60 overflow-hidden"
+                }`}
+            >
+                <p className="text-base mb-4">
+                    {props.body}
+                </p>
+                {!imageError && <img
+                    src={`/api/posts${props.id}/image`}
+                    alt="Post image"
+                    className="rounded-lg w-full mb-4"
+                    onError={() => setImageError(true)}
+                />}
+            </div>
+
+            {!expanded && (
+                <div className="text-center mt-2">
+                    <button
+                        onClick={() => setExpanded(true)}
+                        className="px-4 py-2 bg-white text-gray-800 border border-gray-300 rounded-md shadow-sm hover:bg-gray-100 transition"
+                    >
+                        Read more...
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+}
--- frontend/src/pages/PostsPage.tsx
+++ frontend/src/pages/PostsPage.tsx
@@ -1,3 +1,15 @@
+import Post, {type IPost} from "../components/Post.tsx";
+import {useEffect, useState} from "react";
+
 export default function PostsPage() {
-    return <>Lorem ipsum</>
+    const [posts, setPosts] = useState<IPost[]>([]);
+    useEffect(() => {
+        fetch("/api/posts").then(result => result.json()).then(values => setPosts(values as IPost[]));
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center pt-12">
+            {posts.map(post => <Post {...post}/>)}
+        </div>
+    )
 }
