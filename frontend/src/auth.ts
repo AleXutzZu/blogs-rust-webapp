a09b8a0143f491c99cbdc9c35a827467f6d35815
AleXutzZu <davidalexrobertyt@gmail.com>
2025-06-07 14:31:35 +0300

    Implement auth provider

---
+++ frontend/src/auth.ts
@@ -0,0 +1,85 @@
+import type {LoaderFunction, LoaderFunctionArgs} from "react-router";
+import {redirect} from "react-router"
+
+interface AuthUser {
+    username: string;
+}
+
+interface AuthResult {
+    success: boolean;
+    error?: string;
+}
+
+interface AuthProvider {
+    user: null | AuthUser
+
+    signIn(username: string, password: string): Promise<AuthResult>,
+
+    checkAuth(): Promise<void>,
+
+    signOut(): Promise<AuthResult>,
+
+    isAuthenticated(): boolean,
+}
+
+export const authProvider: AuthProvider = {
+    user: null,
+    async checkAuth() {
+        let response = await fetch("/api/auth/me", {credentials: "include"});
+
+        if (response.ok) {
+            this.user = await response.json() as AuthUser;
+        } else {
+            this.user = null;
+        }
+    },
+
+    async signIn(username: string, password: string): Promise<AuthResult> {
+        let response = await fetch("/api/auth/login", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            })
+        });
+
+        if (!response.ok) return {success: false, error: "Failed to authenticate"};
+
+        await this.checkAuth();
+        return {success: true};
+    },
+
+    async signOut(): Promise<AuthResult> {
+        let response = await fetch("/api/auth/logout", {
+            credentials: "include",
+            method: "POST"
+        });
+
+        if (!response.ok) return {success: false, error: "Failed to logout"};
+
+        this.user = null;
+        return {success: true};
+    },
+
+    isAuthenticated(): boolean {
+        return this.user != null;
+    }
+}
+
+
+export const protectedLoaderWrapper = (loader?: LoaderFunction<any>) => {
+    return async (args: LoaderFunctionArgs) => {
+        if (!authProvider.isAuthenticated()) {
+            let params = new URLSearchParams();
+            params.set("redirectTo", new URL(args.request.url).pathname);
+            return redirect("/login?" + params.toString());
+        }
+        if (typeof loader === "undefined") return;
+
+        return await loader(args);
+    }
+}
