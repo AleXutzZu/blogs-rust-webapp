646825c5cea1d051de492be9525e1a36079d0483
AleXutzZu <davidalexrobertyt@gmail.com>
2025-06-08 23:45:01 +0300

    Refactor login page to use react-hook forms

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -9,14 +9,9 @@ import {
 } from "react-router";
 import {authProvider} from "../auth.ts";
 import * as Yup from "yup";
-import {Form, Formik, type FormikProps} from "formik";
-import Input from "../components/Input.tsx";
-import {useEffect, useRef} from "react";
-
-type AuthForm = {
-    username: string,
-    password: string,
-}
+import {FormProvider, useForm, useFormContext} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {type HTMLInputTypeAttribute, useEffect} from "react";
 
 export async function action({request}: ActionFunctionArgs) {
     const formData = await request.formData();
@@ -30,9 +25,7 @@ export async function action({request}: ActionFunctionArgs) {
         return redirect(redirectTo);
     }
     return {
-        fieldErrors: {
-            username: "Username or password are incorrect"
-        }
+        error: "Username or password are wrong"
     }
 }
 
@@ -40,65 +33,81 @@ export async function loader() {
     if (await authProvider.isAuthenticated()) return redirect("/");
 }
 
+export type AuthForm = {
+    username: string,
+    password: string,
+}
+
+export function AuthInput(props: { name: keyof AuthForm, label: string, type: HTMLInputTypeAttribute }) {
+    const {register, formState: {errors}} = useFormContext<AuthForm>();
+
+    return <>
+        {errors[props.name]?.message &&
+            <label htmlFor={props.name}
+                   className="block text-sm font-medium text-red-400">{errors[props.name]?.message}
+            </label>}
+        {!errors[props.name]?.message &&
+            <label htmlFor="username"
+                   className="block text-sm font-medium text-gray-700">{props.label}</label>}
+        <input {...register(props.name)} className="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg
+                               focus:outline-none focus:ring-2 focus:ring-blue-500" type={props.type}/>
+    </>;
+}
+
 export default function LoginPage() {
     const submit = useSubmit();
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const redirectTo = params.get("redirectTo") || "/";
-    const actionData = useActionData() as { fieldErrors: { username: string } } | undefined;
+    const actionData = useActionData() as { error: string } | undefined;
 
-    const formikRef = useRef<FormikProps<AuthForm> | null>(null);
-
-    useEffect(() => {
-        if (actionData?.fieldErrors && formikRef.current) {
-            formikRef.current.setErrors(actionData.fieldErrors);
-        }
-    }, [actionData]);
-
-    const validationSchema = Yup.object<AuthForm>({
+    const validationSchema = Yup.object({
         username: Yup.string().required("Username is required"),
         password: Yup.string().required("Password is required"),
     });
 
+    const methods = useForm<AuthForm>({
+        resolver: yupResolver(validationSchema),
+        mode: "onBlur",
+    });
+
+    useEffect(() => {
+        methods.setError("username", {message: actionData?.error});
+    }, [actionData]);
+
+
     let navigation = useNavigation();
     let isLoggingIn = navigation.formData?.get("username") != null;
 
+    const onSubmit = async (data: AuthForm) => {
+        await submit({...data, redirectTo}, {method: "post"});
+    }
 
     return (
-        <Formik initialValues={{username: "", password: ""} as AuthForm} onSubmit={async (values) => {
-            await submit({...values, redirectTo}, {method: "post"});
-        }} validationSchema={validationSchema}
-                innerRef={formikRef}
-        >
-            {_formik => (
-                <div className="flex items-center justify-center m-auto min-w-xs">
-                    <Form className="w-full max-w-sm p-6 bg-white rounded-2xl shadow-md space-y-5">
-                        <h2 className="text-2xl font-semibold text-center">Login</h2>
-                        <div>
-                            <Input label="Username" type="text" name="username"
-                                   className="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg
-                               focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                        </div>
-                        <div>
-                            <Input label="Password" type="password" name="password"
-                                   className="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg
-                               focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                        </div>
-
-                        <button type="submit" className="w-full py-2 bg-blue-600 text-white
+        <FormProvider {...methods}>
+            <div className="flex items-center justify-center m-auto min-w-xs">
+                <form className="w-full max-w-sm p-6 bg-white rounded-2xl shadow-md space-y-5"
+                      onSubmit={methods.handleSubmit(onSubmit)}>
+                    <h2 className="text-2xl font-semibold text-center">Login</h2>
+                    <div>
+                        <AuthInput name="username" label="Username" type="text"/>
+                    </div>
+                    <div>
+                        <AuthInput name={"password"} label={"Password"} type={"password"}/>
+                    </div>
+                    <button type="submit" className="w-full py-2 bg-blue-600 text-white
                     rounded-lg hover:bg-blue-700 transition-colors ease-in duration-200"
-                                disabled={isLoggingIn}>{isLoggingIn ? "Logging in..." : "Log In"}
-                        </button>
+                            disabled={isLoggingIn}>{isLoggingIn ? "Logging in..." : "Log In"}
+                    </button>
 
-                        <p className="text-sm text-center text-gray-600">
-                            Don't have an account?{" "}
-                            <Link to="/signup" className="text-blue-600 hover:underline">
-                                Sign up
-                            </Link>
-                        </p>
-                    </Form>
-                </div>
-            )}
-        </Formik>
+                    <p className="text-sm text-center text-gray-600">
+                        Don't have an account?{" "}
+                        <Link to="/signup" className="text-blue-600 hover:underline">
+                            Sign up
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </FormProvider>
     );
 }
