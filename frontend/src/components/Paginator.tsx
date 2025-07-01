9f6f3db8fddd420a413a8884fa3295ef69257cb1
AleXutzZu <davidalexrobertyt@gmail.com>
2025-07-01 14:53:53 +0300

    Replace clip-rule and fill-rule to their JSX counterparts

--- frontend/src/components/Paginator.tsx
+++ frontend/src/components/Paginator.tsx
@@ -153,9 +153,9 @@ export function PostsPaginatorBar(props: PaginatorProps) {
                             <span className="sr-only">Previous</span>
                             <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
                                  data-slot="icon">
-                                <path fill-rule="evenodd"
+                                <path fillRule="evenodd"
                                       d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
-                                      clip-rule="evenodd"/>
+                                      clipRule="evenodd"/>
                             </svg>
                         </button>
                         {pageButtons}
@@ -166,9 +166,9 @@ export function PostsPaginatorBar(props: PaginatorProps) {
                             <span className="sr-only">Next</span>
                             <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
                                  data-slot="icon">
-                                <path fill-rule="evenodd"
+                                <path fillRule="evenodd"
                                       d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                                      clip-rule="evenodd"/>
+                                      clipRule="evenodd"/>
                             </svg>
                         </button>
                     </nav>
